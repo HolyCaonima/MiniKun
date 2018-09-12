aaecf778cf8ae190da8af5d6166c4936d71c3d6f
yuletian <469632477@qq.com>
2018-09-13 00:43:52 +0800

    Full

--- Build/Android/src/com/YourCompany/MiniKun/OBBData.java
+++ Build/Android/src/com/YourCompany/MiniKun/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-84268117L // the length of the file in bytes
+120895866L // the length of the file in bytes
 )
 };
 };
